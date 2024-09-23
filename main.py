7882c33717615a19634dd61ba54f459797855c50
YangTh599 <YangTh599@northwested.org>
2024-09-23 11:09:25 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+# Thayer Yang
+# 23 SEP 2024
+# Pseudocode Practice
+
+
+#Asking for inputs for base and height
+base = float(input('Enter a value for the base of the triangle: \n'))
+height = float(input('Enter a value for the height of the triangle: \n'))
+
+#Getting area of the triangle
+
+area = (base*height)/2
+
+#Printing outputs
+
+print(f'A triangle with a base of {base} units and height of {height} units has an area of {area} units squared')
